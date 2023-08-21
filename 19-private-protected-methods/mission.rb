035c7f991e6a369111b290e059271b1f0474ddec
Nathallye Bacelar <nathallyet@gmail.com>
2023-08-20 21:34:22 -0300

    Private mission

--- 19-private-protected-methods/mission.rb
+++ 19-private-protected-methods/mission.rb
@@ -1,17 +1,17 @@
-class Carro
+class Car
   def get_km(phrase)
     km = find_km(phrase) # find_km é o método private que vai receber a frase e vai procurar o casamento de padrão
-    puts km # Imprime o casamento da expressão regular
+    puts km # imprime o casamento da expressão regular
   end
   
   private
 
   def find_km(phrase)
-    /\d{2}km\/h/.match(phrase) # Retorno do método private find_km
+    /\d{2}km\/h/.match(phrase) # retorno do método private find_km
   end
 end
 
 phrase = 'Um fusca de cor amarela viaja a 80km/h'
 
-car = Carro.new
+car = Car.new
 car.get_km(phrase)
