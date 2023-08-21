d491028ab26555b0a1543791dac2874e6cba314a
Nathallye Bacelar <nathallyet@gmail.com>
2023-08-20 21:32:36 -0300

    Regex mission

--- 16-regex/mission.rb
+++ 16-regex/mission.rb
@@ -3,6 +3,6 @@ standard = /\(\d{2}\) \d \d{4}-\d{4}/.match(text)
 
 puts 'O objeto:'
 p standard # Printa o objeto 
-puts ''
-puts 'Valor do objeto:'
+
+puts "\nValor do objeto:"
 puts standard # Printa o que tem dentro do objeto
